eab5960df567af3f6163aaee6238361bb834e328
arman-sh <rosko1245@gmail.com>
2019-06-13 21:00:15 +0300

    def

--- src/main/java/pages/group_page/cards/MemberCard.java
+++ src/main/java/pages/group_page/cards/MemberCard.java
@@ -1,16 +1,24 @@
 package pages.group_page.cards;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 public final class MemberCard {
 
-    private final String userName;
+    private static final By NAME_LOCATOR = By.xpath("//*[@class='user-grid-card']/div[2]/div[1]/a");
+
+    private String userName;
 
     public MemberCard(final WebElement webElement) {
-        userName = webElement
-                .findElement(By.xpath("//*[@class='user-grid-card']/div[2]/div[1]/a"))
-                .getAttribute("innerHTML");
+        try {
+            userName = webElement
+                    .findElement(NAME_LOCATOR)
+                    .getAttribute("innerHTML");
+        }
+        catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getUserName() {
